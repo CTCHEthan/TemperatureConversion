fb08860821174c317c9a0df64649d6b56b1e3956
Howell <eh276017@ohio.edu>
2018-09-20 19:11:55 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    //Prompt user for Celsius
+
+    printf("Please enter degrees in Celsius ");
+
+    //Accept user input and store in Celsius variable
+    float celsius;
+    scanf("%f", &celsius);
+
+    //Perform temp conversion
+    float fahrenheit;
+    fahrenheit = ( ( celsius * 9) / 5) + 32 ;
+
+
+
+
+    //Display results
+    printf("%.2f F \n",fahrenheit);
+    return 0;
+}
